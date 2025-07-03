c71bd6af03d03a7c60e3eacf0c371e79c92df6fd
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-07-03 02:17:54 -0300

    Update input.jsx

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-export function Input(props) {
+export function Input({ className = "", ...props }) {
   return (
     <input
+      className={`border rounded px-3 py-2 w-full focus:outline-none focus:ring-2 focus:ring-indigo-500 ${className}`}
       {...props}
-      className={`border border-gray-300 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${props.className || ''}`}
     />
   );
 }
