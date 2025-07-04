4160f71a6225b25429374f3def69cf962f099865
henriquesilvaprimme <henriquesilva.primme@gmail.com>
2025-07-04 15:56:27 -0300

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -10,7 +10,7 @@ const Dashboard = ({ leads }) => {
   const buscarLeads = async () => {
     try {
       const response = await axios.get(
-        'https://script.google.com/macros/s/AKfycbzJ_WHn3ssPL8VYbVbVOUa1Zw0xVFLolCnL-rOQ63cHO2st7KHqzZ9CHUwZhiCqVgBu/exec?v=pegar_clientes_fechados'
+        'https://script.google.com/macros/s/AKfycbwDRDM53Ofa4o5n7OdR_Qg3283039x0Sptvjg741Hk7v0DXf8oji4aBpGji-qWHMgcorw/exec?v=pegar_clientes_fechados'
       );
       setLeads(response.data);
     } catch (error) {
